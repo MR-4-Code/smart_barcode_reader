0a4aa51fbceb0c01bef80cf64ec254addaa15571
Mohammad Abureesh <m7amdabureesh@gmail.com>
2025-12-24 20:36:13 +0200

    [#1] fix lose focus node and add it as optional params

--- lib/src/smart_barcode_reader_widget.dart
+++ lib/src/smart_barcode_reader_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smart_barcode_reader/smart_barcode_reader.dart';
+
 import 'smart_barcode_reader/smart_barcode_reader.dart';
 
 /// A widget that listens for keyboard events and processes them as barcode input.
@@ -10,16 +11,24 @@ import 'smart_barcode_reader/smart_barcode_reader.dart';
 ///
 /// Usage:
 /// ```dart
-/// SmartBarCodeScannerWidget(
+/// SmartBarCodeReaderWidget(
 ///   onBarcodeDetected: (code) => print('Barcode: $code'),
 ///   child: MyWidget(),
 /// )
 /// ```
+///
+/// [focusNode] is optional params
 class SmartBarCodeReaderWidget extends StatefulWidget {
   final SmartBarCodeReaderOptions? options;
   final Widget? child;
+  final FocusNode? focusNode;
 
-  const SmartBarCodeReaderWidget({super.key, this.child, this.options});
+  const SmartBarCodeReaderWidget({
+    super.key,
+    this.child,
+    this.options,
+    this.focusNode,
+  });
 
   @override
   State<SmartBarCodeReaderWidget> createState() =>
@@ -28,32 +37,33 @@ class SmartBarCodeReaderWidget extends StatefulWidget {
 
 class _SmartBarCodeReaderWidgetState extends State<SmartBarCodeReaderWidget> {
   late final SmartBarCodeReader _reader;
+  late final FocusNode focusNode;
 
   @override
   void initState() {
     super.initState();
+    focusNode = widget.focusNode ?? FocusNode();
+
     final options = widget.options;
 
     //To setup barcode reader from [options]
     _reader = SmartBarCodeReader(
-        onBarcodeDetected: (code) {
-          if (code.isNotEmpty) {
-            debugPrint('Barcode detected: $code');
-            options?.onBarcodeDetected?.call(code);
-          }
-        },
-        maxLength: options?.maxLength,
-        minLength: options?.minLength,
-        tolerance: options?.tolerance,
-        feedbackManager: options?.userFeedbackManager,
-        maxInterKeyDurationMs: options?.maxInterKeyDurationMs ?? 20,
-        validCharPattern: options?.validCharPattern ?? RegExp(r'^[0-9]+$'),
-        supportedFormats: options?.supportedFormats ??
-            {
-              BarcodeFormat.ean13,
-              BarcodeFormat.upcA,
-              BarcodeFormat.code128,
-            });
+      onBarcodeDetected: (code) {
+        if (code.isNotEmpty) {
+          debugPrint('Barcode detected: $code');
+          options?.onBarcodeDetected?.call(code);
+        }
+      },
+      maxLength: options?.maxLength,
+      minLength: options?.minLength,
+      tolerance: options?.tolerance,
+      feedbackManager: options?.userFeedbackManager,
+      maxInterKeyDurationMs: options?.maxInterKeyDurationMs ?? 20,
+      validCharPattern: options?.validCharPattern ?? RegExp(r'^[0-9]+$'),
+      supportedFormats:
+          options?.supportedFormats ??
+          {BarcodeFormat.ean13, BarcodeFormat.upcA, BarcodeFormat.code128},
+    );
   }
 
   @override
@@ -64,11 +74,11 @@ class _SmartBarCodeReaderWidgetState extends State<SmartBarCodeReaderWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final node = FocusNode();
     return KeyboardListener(
-        autofocus: true,
-        focusNode: node,
-        onKeyEvent: _reader.handleKeyEvent,
-        child: widget.child ?? const SizedBox());
+      autofocus: true,
+      focusNode: focusNode,
+      onKeyEvent: _reader.handleKeyEvent,
+      child: widget.child ?? const SizedBox(),
+    );
   }
 }
